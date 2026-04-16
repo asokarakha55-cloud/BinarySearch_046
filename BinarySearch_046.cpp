a11985f3ee88053a4dfd7666482c2f4a659c5722
asokarakha55-cloud <asokarakha55@gmail.com>
2026-04-16 14:58:22 +0700

    menambahkan fungsi ke-1

--- BinarySearch_046.cpp
+++ BinarySearch_046.cpp
@@ -7,4 +7,18 @@ int x;
 
 void input()
 {
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array (maksimal 10) :";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. silakan coba lagi.\n";
+        }
+    }
 }
